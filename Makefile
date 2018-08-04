7ff3868ea05a0e721a69cb7888e693b0976f0b96
shigeru yokochi <shigeru@yokochi.jp>
2018-08-04 12:29:24 +0900

    new

---
+++ Makefile
@@ -0,0 +1,4 @@
+﻿all:
+	gcc -o main main.c hmc5883l.c -lm
+clean:
+	rm -f  main
---
+++ main.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <unistd.h>
+
+extern int HMC5883L_init(void);
+extern int HMC5883L_GetDirection(void);
+
+/**************************************
+*	sample
+*************************************/
+int main(int argc,	char **argv)
+{
+	int i;
+
+	if (HMC5883L_init() == 1) {
+		printf("*** HMC5883L_init() error.\n");
+		return -1;
+	}
+
+
+	for (i = 0; i < 100; i++) {
+		printf("%d\n",HMC5883L_GetDirection());
+		usleep(1000 * 100);
+	}
+
+	return 0;
+}
